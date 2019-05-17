1141da4554dc9b280b00faf235ebc19d79000a7f
lilu <lilu@zenmen.com>
2019-05-17 18:12:44 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Simulator from "./packages/simulator/index.js";
+import Button from "./packages/button/index.js";
+import D3box from "./packages/3dbox/index.js";
+import Bar from "./packages/bar/index.js";
+import Input from "./packages/input/index.js";
+import Loading from "./packages/loading/index.js";
+
+const components = [Simulator, Button, Bar, Input, D3box];
+const plugins = [Loading];
+
+const install = function(Vue, opts = {}) {
+  components.map(component => {
+    Vue.component(component.name, component);
+  });
+  plugins.map(plugin => {
+    Vue.use(plugin);
+  });
+};
+
+/* 支持使用标签的方式引入 */
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+}
+
+export default {
+  install,
+  Simulator,
+  Button,
+  Bar
+};
